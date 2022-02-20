9a17cfe4c4664353b3cc6cfab62253829910819d
Denise Moscon <denise.moscon@vidiemme.it>
2022-02-20 16:36:58 +0100

    Feat(resume): indent and style

--- src/components/pages/Resume/Resume.jsx
+++ src/components/pages/Resume/Resume.jsx
@@ -7,41 +7,51 @@ const Resume = () => {
         <div className="resume">
             <p>
                 <BiPlanet /> <b>Denise Moscon</b><br />
-                <span>27 yo</span><br />
-                <span>Front-end developer</span><br />
-                <span>Web enthusiast, pathological curious, bulimic reader, oddities lover.</span>
+                <div className="resume__indent">
+                    <span>27 yo</span><br />
+                    <span>Front-end developer</span><br />
+                    <span>Web enthusiast, pathological curious, bulimic reader, oddities lover.</span>
+                </div>
             </p>
             <p>
                 <BiAtom /> <b>Studies</b><br />
-                <span>2016-2019</span>
-                <span>Scuola Internazionale di Comics (Padova, Italy)</span><br />
-                <span>Graphic and web design, web development</span><br />
+                <div className="resume__indent" >
+                    <i>2016-2019</i><br />
+                    <span>Scuola Internazionale di Comics (Padova, Italy)</span><br />
+                    <span>Graphic and web design, web development</span><br />
+                </div>
             </p>
             <p>
                 <BiGame /> <b>Career</b><br />
-                <span>06/2019 - 12/2019</span><br />
-                <span>Happy Brain (Verona, Italy)</span><br />
-                <span>Front-end development (React, React Native, Wordpress)</span><br />
-                <br />
-                <span>12/2019 - 03/2021</span><br />
-                <span>Skill srl (Verona, Italy)</span><br />
-                <span>Front-end development (React)</span><br />
-                <br />
-                <span>03/2021 - now</span><br />
-                <span>Vidiemme consulting (Milano, Italy)</span><br />
-                <span>Front-end development (React, React Native, landing pages)</span><br />
-                <br />
+                <div className="resume__indent">
+                    <i>06/2019 - 12/2019</i><br />
+                    <span>Happy Brain (Verona, Italy)</span><br />
+                    <span>Front-end development (React, React Native, Wordpress)</span><br />
+                    <br />
+                    <i>12/2019 - 03/2021</i><br />
+                    <span>Skill srl (Verona, Italy)</span><br />
+                    <span>Front-end development (React)</span><br />
+                    <br />
+                    <i>03/2021 - now</i><br />
+                    <span>Vidiemme consulting (Milano, Italy)</span><br />
+                    <span>Front-end development (React, React Native, landing pages)</span><br />
+                    <br />
+                </div>
+
             </p>
             <p>
                 <BiRocket /> <b>Skills</b><br />
-                <span>React, React Native, CRA</span><br />
-                <span>Redux and such</span><br />
-                <span>HTML5 (Canvas, SVG), CSS3, SASS, SCSS, Javascript (ES6+)</span><br />
-                <span>Rest API</span><br />
-                <span>Bootstrap, GSAP, Jquery and more libraries</span><br />
-                <span>Git, npm, yarn, composer, parcel, webpack</span><br />
-                <span>CI/CD</span><br />
-                <span>Suite Adobe, Sketch, Affinity Design</span>
+                <div className="resume__indent">
+                    <span>React, React Native, CRA</span><br />
+                    <span>NextJs</span><br />
+                    <span>Redux and such</span><br />
+                    <span>HTML5 (Canvas, SVG), CSS3, SASS, SCSS, Javascript (ES6+)</span><br />
+                    <span>Rest API</span><br />
+                    <span>Bootstrap, GSAP, Jquery and more libraries</span><br />
+                    <span>Git, npm, yarn, composer, parcel, webpack</span><br />
+                    <span>CI/CD</span><br />
+                    <span>Suite Adobe, Sketch, Affinity Design</span>
+                </div>
             </p>
         </div>
     )
