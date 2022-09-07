17eb3ed6b860138ba78f7f7600214a28b19ca302
Denise Moscon <denise.moscon@vidiemme.it>
2022-09-07 14:42:07 +0200

    Fix: missing url

--- src/components/molecules/HeaderMenu/HeaderMenu.jsx
+++ src/components/molecules/HeaderMenu/HeaderMenu.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import './HeaderMenu.scss';
 import MenuLink from "../../atoms/MenuLink/MenuLink";
-import {NavLink} from "react-router-dom";
 
 const HeaderMenu = () => {
     return (
@@ -9,12 +8,6 @@ const HeaderMenu = () => {
             <MenuLink link="/" label="Me"/>
             <MenuLink link="/resume" label="What have I done with my life" />
             <MenuLink link="/contacts" label="Where to find me (if you must!)" />
-            <NavLink
-                style={{display: "none"}}
-                to={'/appetta'}
-            >
-                appetta
-            </NavLink>
         </div>
     )
 }
