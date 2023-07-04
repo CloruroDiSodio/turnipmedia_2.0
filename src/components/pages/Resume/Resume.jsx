5ad05bbbbd322097eb5779be6c56888b73f8b506
Denise Moscon <den.moscon@gmail.com>
2023-07-04 15:34:20 +0200

    feat(resume): update info

--- src/components/pages/Resume/Resume.jsx
+++ src/components/pages/Resume/Resume.jsx
@@ -8,9 +8,32 @@ const Resume = () => {
             <p>
                 <BiPlanet /> <b>Denise Moscon</b><br />
                 <div className="resume__indent">
-                    <span>27 yo</span><br />
+                    <span>29 yo</span><br />
                     <span>Front-end developer</span><br />
                     <span>Web enthusiast, pathological curious, bulimic reader, oddities lover.</span>
+                </div>
+            </p>
+
+            <p>
+                <BiGame /> <b>Career</b><br />
+                <div className="resume__indent">
+                    <i>03/2023 - now</i><br />
+                    <span>Arduino (Torino, Italy)</span><br />
+                    <span>Front-end development (React.js, Next.js, landing pages)</span><br />
+                    <br />
+                    <i>03/2021 - 03/2023</i><br />
+                    <span>Vidiemme consulting (Milano, Italy)</span><br />
+                    <span>Front-end development (React.js, React Native, landing pages)</span><br />
+                    <span>Front-end mentoring</span><br />
+                    <br />
+                    <i>12/2019 - 03/2021</i><br />
+                    <span>Skill srl (Verona, Italy)</span><br />
+                    <span>Front-end development (React.js)</span><br />
+                    <br />
+                    <i>06/2019 - 12/2019</i><br />
+                    <span>Happy Brain (Verona, Italy)</span><br />
+                    <span>Front-end development (React.js, React Native, Wordpress)</span><br />
+                    <br />
                 </div>
             </p>
             <p>
@@ -22,28 +45,11 @@ const Resume = () => {
                 </div>
             </p>
             <p>
-                <BiGame /> <b>Career</b><br />
-                <div className="resume__indent">
-                    <i>06/2019 - 12/2019</i><br />
-                    <span>Happy Brain (Verona, Italy)</span><br />
-                    <span>Front-end development (React, React Native, Wordpress)</span><br />
-                    <br />
-                    <i>12/2019 - 03/2021</i><br />
-                    <span>Skill srl (Verona, Italy)</span><br />
-                    <span>Front-end development (React)</span><br />
-                    <br />
-                    <i>03/2021 - now</i><br />
-                    <span>Vidiemme consulting (Milano, Italy)</span><br />
-                    <span>Front-end development (React, React Native, landing pages)</span><br />
-                    <br />
-                </div>
-
-            </p>
-            <p>
                 <BiRocket /> <b>Skills</b><br />
                 <div className="resume__indent">
-                    <span>React, React Native, CRA</span><br />
+                    <span>React.js, React Native, CRA</span><br />
                     <span>NextJs</span><br />
+                    <span>Testing libraries (Playwright)</span><br />
                     <span>Redux and such</span><br />
                     <span>HTML5 (Canvas, SVG), CSS3, SASS, SCSS, Javascript (ES6+)</span><br />
                     <span>Rest API</span><br />
